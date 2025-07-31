ed019d78ae0faa001ed3b6cc6c65d5eda2decb46
Lawrence-wambugu <wambugulawrence@gmail.com>
2025-07-31 19:43:49 +0300

    Reverted to proper app after database initialization

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import the Flask app from server directory
-from test_app import app
+from server.app_simple import app
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000) 
